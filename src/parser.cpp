8cf3c2d57f5edee00275c7fce00445f049bf06aa
Cooper Frank <coopfrank04@gmail.com>
2025-09-09 22:06:09 -0400

    Fixed off-by-one errors.

--- src/parser.cpp
+++ src/parser.cpp
@@ -3,10 +3,15 @@
 
 namespace StringUtils {
     void parseName(const std::string& fullName, std::string* firstName, std::string* lastName) {
+        size_t pos = fullName.find(" ");
 
+        *firstName = fullName.substr(0, pos);
+        *lastName = fullName.substr(pos+1);
     }
 
     std::string getUsername(const std::string& email) {
+        size_t pos = email.find("@");
 
+        return email.substr(0, pos);
     }
 }
